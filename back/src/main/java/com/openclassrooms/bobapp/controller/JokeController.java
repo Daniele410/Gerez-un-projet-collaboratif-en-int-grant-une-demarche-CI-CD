3eeae2e2ecb15152b6c7f6cae2e8bd4fc0e25902
Daniele410 <miraglia.daniele410@gmail.com>
2024-05-31 18:31:48 +0200

    test push

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -1,12 +1,10 @@
 package com.openclassrooms.bobapp.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.openclassrooms.bobapp.service.JokeService;
-
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RestController
 @RequestMapping("api/joke")
 public class JokeController {
